ab7f091a97e2ede7bbf5fbda29282adec5652159
Sam Miller <smil@lle.rochester.edu>
2024-02-16 16:49:16 -0500

    remove @show statements

--- src/halo.jl
+++ src/halo.jl
@@ -8,13 +8,11 @@ function get_haloedge_regions(domain::CartesianIndices{N}, axis::Int, nhalo::Int
   # Shrink by nhalo along all axes to get the 
   # non-halo region in the "center" of the domain
   inner_domain = expand(domain, -nhalo)
-  @show inner_domain
 
   # edge regions
   edge_lo = extract_from_lower(inner_domain, axis, nhalo)
   edge_hi = extract_from_upper(inner_domain, axis, nhalo)
 
-  @show edge_lo edge_hi
   halo_lo = shift(edge_lo, axis, -nhalo)
   halo_hi = shift(edge_hi, axis, +nhalo)
 
