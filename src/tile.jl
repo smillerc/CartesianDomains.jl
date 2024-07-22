6bba2a8221af81bc62b384a345aaa6221ae9cef3
Miller, Samuel <smil@lle.rochester.edu>
2024-07-22 15:56:00 -0400

    fix the tile command

--- src/tile.jl
+++ src/tile.jl
@@ -7,8 +7,13 @@ using .Iterators: partition, product
 Split `domain` up by `fraction` into smaller chunks. This is designed to split a 
 CartesianIndex into subdomains. `fraction` can be a single `Int` or a `NTuple{Int}`
 """
-function tile(domain, fraction)
+function tile(domain, fraction; cell_based=true)
   splits = cld.(size(domain), fraction)
-  tiles = product(partition.(axes(domain), splits)...)
-  return collect(tiles)
+  tiles = collect(CartesianIndices.(product(partition.(axes(domain), splits)...)))
+
+  if cell_based
+    return tiles
+  else
+    return expand_upper.(tiles, 1)
+  end
 end
