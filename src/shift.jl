2822a15502c1b5c121568419e7573cdf7e8c657b
Miller, Samuel <smil@lle.rochester.edu>
2024-11-07 09:52:38 -0500

    add a docstring

--- src/shift.jl
+++ src/shift.jl
@@ -21,6 +21,12 @@ function shift(domain::CartesianIndices{N}, axis::Int, n::Int) where {N}
   return CartesianIndices(idx)
 end
 
+"""
+    shift(domain::CartesianIndices{N}, axes::NTuple{N2,Int}, n::NTuple{N2,Int}
+) where {N,N2}
+
+Shift a `CartesianIndices` domain by `n` where n is a Tuple on given set of `axes`
+"""
 function shift(
   domain::CartesianIndices{N}, axes::NTuple{N2,Int}, n::NTuple{N2,Int}
 ) where {N,N2}
